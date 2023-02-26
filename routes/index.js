ed6347da270e518e43a2d87342eda12c2ba85d84
Nathanim1919 <nathanim2tadele@gmail.com>
2023-02-26 14:11:38 +0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini-Message-Board', messages : messages });
+});
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini-Message-Board', messages : messages });
+});
+
+router.post('/new', function(req, res, next) {
+  if (req.body.messageText == '' || req.body.userName =='') {
+   res.redirect('/')
+  }else{
+
+    messages.push({text: req.body.messageText, user: req.body.userName, added: new Date()});
+    res.redirect('/')
+  }
+});
+
+
+module.exports = router;
